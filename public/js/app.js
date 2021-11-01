bfb539e7cb63a01f88228af2cc5c460b43a248fc
Jawahar Prudhvi Rayudu Anaparthi <Jawahar.Prudhvi.Rayudu.Anaparthi@gds.ey.com>
2021-11-01 17:09:50 +0530

    Removing unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log('This is client side java script.')
-
-// fetch('http://localhost:3000/weather?address=Kanuru').then((response) =>{
-//     response.json().then((data) =>{
-//         if(data.error){
-//             console.log(data.error)
-//         }else{
-//             console.log(data.location);
-//             console.log(data.forecast);
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const msgOne = document.querySelector('#msg-1');

4015580051d91a202271c7ab5a5b9f960c480e28
Jawahar Prudhvi Rayudu Anaparthi <Jawahar.Prudhvi.Rayudu.Anaparthi@gds.ey.com>
2021-11-02 13:31:01 +0530

    set up app for heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     msgOne.textContent = 'loading...';
     msgTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    fetch('/weather?address=' + search.value).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msgOne.textContent = data.error;
